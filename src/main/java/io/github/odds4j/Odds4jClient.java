3e85d59ed6ec5e9dadff178e18aa3d7efc044190
isvora <isvoranandrei25@gmail.com>
2022-11-29 16:23:51 +0200

    Javadoc

--- src/main/java/io/github/odds4j/Odds4jClient.java
+++ src/main/java/io/github/odds4j/Odds4jClient.java
@@ -33,6 +33,10 @@ public class Odds4jClient {
         this.odds4jService = odds4jService;
     }
 
+    /**
+     * Used to get a list of all the sports currently supported by the-odds-api
+     * @return List of Sport objects
+     */
     public List<Sport> getSports() {
         try {
             return odds4jService.getSports().orElse(Collections.emptyList());
@@ -41,6 +45,13 @@ public class Odds4jClient {
         }
     }
 
+    /**
+     * Get all available odds for a given sport, in a region specified and for a market specified.
+     * @param sport the sport key you get by calling getSports() method. Example: "americanfootball_ncaaf"
+     * @param regions the region you want to get results from. Example "us"
+     * @param markets the markets to get odds for. Example "h2h"
+     * @return
+     */
     public List<Odds> getOdds(String sport, String regions, String markets) {
         try {
             return odds4jService.getOdds(sport, regions, markets).orElse(Collections.emptyList());
@@ -49,6 +60,13 @@ public class Odds4jClient {
         }
     }
 
+    /**
+     * Gets the scored for a given sport. Optionally you can add daysFrom to get historical scores from the last 3 days
+     * If you don't specify daysFrom, you will get live data only.
+     * @param sport the sport key you get by calling getSports() method. Example: "americanfootball_ncaaf"
+     * @param daysFrom integer (1 to 3)
+     * @return
+     */
     public List<Match> getScores(String sport, Optional<Integer> daysFrom) {
         try {
             return odds4jService.getScores(sport, daysFrom).orElse(Collections.emptyList());
@@ -56,6 +74,15 @@ public class Odds4jClient {
             return Collections.emptyList();
         }
     }
+
+    /**
+     * Get all available historical odds for a given sport, in a region specified and for a market specified.
+     * In order to use this, you need to have a paid subscription to the-odds-api.
+     * @param sport the sport key you get by calling getSports() method. Example: "americanfootball_ncaaf"
+     * @param regions the region you want to get results from. Example "us"
+     * @param markets the markets to get odds for. Example "h2h"
+     * @return
+     */
 
     public Optional<HistoricalOdds> getHistoricalOdds(String sport, String regions, String markets) {
         try {
@@ -65,6 +92,10 @@ public class Odds4jClient {
         }
     }
 
+    /**
+     * Method used to create a builder instance
+     * @return Odds4jClientBuilder instance
+     */
     public static Odds4jClientBuilder builder() {
         return new Odds4jClientBuilder();
     }
@@ -73,6 +104,11 @@ public class Odds4jClient {
 
         private String apiKey;
 
+        /**
+         * Method used to store the api key into your Odds4jClient.
+         * @param apiKey
+         * @return instance of Odds4jClientBuilder with api key set.
+         */
         public Odds4jClientBuilder apiKey(String apiKey) {
             this.apiKey = apiKey;
             return this;
